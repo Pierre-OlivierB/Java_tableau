cc216595b65b136c076f09bd7bc5a243007e721b
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-11 15:44:59 +0100

    ajout temps aux actions

--- Tamagochi_v2/src/TableauAnimal.java
+++ Tamagochi_v2/src/TableauAnimal.java
@@ -6,16 +6,16 @@ public class TableauAnimal {
 		/*Configuration de l'animal*/
 		Animal denver= new Animal("Denver", 10, 1);
 		/*Configuration du temps*/
-		Temps clock=new Temps(0,"Jeune",0);
+		Temps horloge=new Temps(0,"Jeune",0);
 		System.out.println("Ton dinosaur s'appelle: "+denver.getNom());
 		boolean vivant=false;
 		/*boucle d'action de l'animal*/
 		do {
 			/*statut(denver);*/
 			vivant=true;
-			/*affichage de l'état puis mise en action*/
-			montreEtat(denver);
-			actions(denver);
+			/*affichage de l'état puis mise en action et du temps qui passe*/
+			montreEtat(denver, horloge);
+			actions(denver, horloge);
 			/*vérifications des constantes*/
 			if(denver.getEnergie()<1) {
 				System.out.println(denver.getNom()+"  se sent à bout d'énergie.");
@@ -32,33 +32,37 @@ public class TableauAnimal {
 		System.out.println(denver.getNom()+" est mort! Paix à son âme.");
 	}
 	/*Présentation de l'état de l'animal*/
-	public static void montreEtat(Animal animal) {
-		System.out.println("Actuellement "+animal.getNom()+" a "+animal.getEnergie()+" d'énergie.");	
+	public static void montreEtat(Animal animal, Temps passe) {
+		System.out.println("Actuellement "+animal.getNom()+" a "+animal.getEnergie()+" d'énergie.");
+		int jourVie=passe.getJourDeVie();
+		int journee=passe.getTempsPasse();
+		String nom=animal.getNom();
+		System.out.println(nom+" a "+jourVie+" jours de vie. Dans le monde de "+nom+" il est "+journee+"h!");
 	}
 	/*propositions d'actions*/
-	public static void actions(Animal animal) {
+	public static void actions(Animal animal,Temps passe) {
 		System.out.println("Que voulez-vous faire avec "+animal.getNom()+" ?");
 		System.out.println("Réponse 1: Manger; 2: Jouer; 3: Dormir");
 		int choix=Clavier.lireInt();
 		if(choix==1) {
 			System.out.println("Vous avez choisi de nourrir "+animal.getNom()+", il est content!");
-			mangerAnimal(animal);
+			mangerAnimal(animal,passe);
 		}
 		else if(choix==2) {
 			System.out.println("Vous avez choisi de jouer avec "+animal.getNom()+", il est content!");
-			jouerAnimal(animal);
+			jouerAnimal(animal,passe);
 		}
 		else if(choix==3) {
 			System.out.println("Vous avez choisi de lui demander d'aller se coucher. "+animal.getNom()+" le fait à reculon!");
 			if(animal.getEnergie()>15) {
 				System.out.println(animal.getNom()+"ne vous écoute pas, il veut faire autre chose!");
-				actions(animal);
+				actions(animal,passe);
 			}
-			dormirAnimal(animal);
+			dormirAnimal(animal,passe);
 		}
 		else {
 			System.out.println(animal.getNom()+" n'a pas bien compris ce que tu voulais faire.");
-			actions(animal);
+			actions(animal,passe);
 		}
 	}
 	/*calcul de l'énergie perdue*/
@@ -67,24 +71,30 @@ public class TableauAnimal {
 		int r= Random();
 		animal.setEnergie(n-r);
 	}
-	/*poids-- grâce à actions*/
-	public static void jouerAnimal(Animal animal) {
+	/*poids-- grâce à actions & temps++, 
+	 * plus de temps passé plus de poids perdu*/
+	public static void jouerAnimal(Animal animal,Temps passe) {
 		System.out.println(animal.getNom()+" court partout");
 		System.out.println(animal.getNom()+" sort sa guitare et joue un morceau.");
 		energieAnimal(animal);
 		int p=animal.getPoids();
+		int h=passe.getTempsPasse();
 		int r = Random();
+		passe.setTempsPasse(h+r);
 		p=p-r;
 		if(p<1) {
 			p=1;
 		}
 		animal.setPoids(p);
 	}
-	/*domir = energie++*/
-	public static void dormirAnimal(Animal animal) {
+	/*domir = energie++; dormir = temmps++ +1*/
+	public static void dormirAnimal(Animal animal, Temps passe) {
 		System.out.println(animal.getNom()+" va se coucher");
 		int h=animal.getEnergie();
-		h=h+Random();
+		int p=passe.getTempsPasse();
+		int ran=Random();
+		h=h+ran;
+		passe.setTempsPasse(p+ran+1);
 		animal.setEnergie(h);
 		System.out.println(animal.getNom()+" dort toujours.");
 		System.out.println(animal.getNom()+" est à "+animal.getEnergie()+" d'energie!");
@@ -105,13 +115,15 @@ public class TableauAnimal {
 		}
 }
 	/*Manger = poids++ && energie++*/
-	public static void mangerAnimal(Animal animal) {
+	public static void mangerAnimal(Animal animal, Temps passe) {
 		System.out.println(animal.getNom()+" vous tourne autour pour trouver la nourriture.");
 		System.out.println("Vous finnissez par lui en donner.");
 		System.out.println(animal.getNom()+" vous sourrit!");
 		int m=animal.getEnergie();
 		int r=Random();
 		m=m+r;
+		int h=passe.getTempsPasse();
+		passe.setTempsPasse(h+r);
 		if(r>3) {
 			System.out.println("Attention l'animal a beaucoup mangé.");
 			}
@@ -127,6 +139,17 @@ public class TableauAnimal {
 		animal.setPoids(volume+quantite);
 		System.out.println("Attention "+animal.getNom()+" prends du poids.");
 	}
+	/*affichage du temps qui passe*/
+//	public void affichageDuTemps(Animal animal,Temps passe) {
+//		
+//	}
+	
+	/*temps jouer*/
+//	public void tempsJouer() {
+//		int jouer = 
+//	}
+	/*temps dormir*/
+	
 	/*Calcul du random*/
 	public static int Random() {
 		int max=5;
