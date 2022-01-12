2fcc49e10df6a816c0d81f3ca80a2f88b45acaf9
BAUDINO PIERRE <PIE.BAUDINO@pedago.mermoz.edu>
2022-01-12 14:47:24 +0100

    ajout dossier tp 9

---
+++ TP9_Interfaces/src/C_CERCLE.java
@@ -0,0 +1,17 @@
+
+public class C_CERCLE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Les attributs sont : les coordonnées x et y du centre du cercle
+								//le rayon
+		
+		//Les méthodes sont : 
+		//C_CERCLE (abs, ord, rayon)
+		//déplace (delta_x, delta_y)
+		//affiche () // utilisez un simple appel à « println »
+		//boolean compare ( cercle ) // comparaison des rayons uniquement
+		//double surface()
+	}
+
+}
