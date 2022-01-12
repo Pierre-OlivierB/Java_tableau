ea751a47393998f9b3d59e6a1153c21c6b30b34b
BAUDINO PIERRE <PIE.BAUDINO@pedago.mermoz.edu>
2022-01-12 17:16:39 +0100

    ajout fin exo2 tp9

--- TP9_Interfaces/src/C_CERCLE.java
+++ TP9_Interfaces/src/C_CERCLE.java
@@ -1,35 +1,60 @@
 
 public class C_CERCLE {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Les attributs sont : les coordonnées x et y du centre du cercle
-								//le rayon
-		int x;
-		int y;
-		int rayon;
-		
-		
-		//Les méthodes sont : 
-		//C_CERCLE (abs, ord, rayon)
-		//déplace (delta_x, delta_y)
-		//affiche () // utilisez un simple appel à « println »
-		//boolean compare ( cercle ) // comparaison des rayons uniquement
-		//double surface()
+	
+	int x;
+	int y;
+	int r;
+	
+	public int getX() {
+		return x;
 	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getR() {
+		return r;
+	}
+	public void setR(int r) {
+		this.r = r;
+	}
+	
 	public C_CERCLE(int abs, int ord, int rayon) {
-		
+		this.x=abs;
+		this.y=ord;
+		this.r=rayon;
 	}
 	public void deplace(int delta_x, int delta_y) {
-		
+		this.x=delta_x;
+		this.y=delta_y;
 	}
 	public void affiche() {
-	
+		System.out.println("La position de x est: "+x);
+		System.out.println("La position de y est: "+y);
 	}
+//	public void affiche(int position) {
+//		System.out.println("La position de x est: "+position);
+//		System.out.println("La position de y est: "+y);
+//	}
 	public boolean compare(int cercle) {
-		return true;
+		System.out.println("Les rayons comparés sont: "+cercle+" et "+r);
+		if (r>cercle) {
+			System.out.println("Le rayon de base est plus grand que le rayon donné");	
+			return true;
+		}
+		else {
+			System.out.println("Le rayon de base est plus petit ou égal que le rayon donné");
+			return false;
+		}
+		
 	}
 	public double surface() {
-		return 2.2;
+		double s=2*(3.14)*(r^2);
+		return s;
 	}
 }
---
+++ TP9_Interfaces/src/MainApp.java
@@ -0,0 +1,47 @@
+
+public class MainApp {
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Les attributs sont : les coordonnées x et y du centre du cercle
+								//le rayon
+		C_CERCLE test=new C_CERCLE(5, 10, 5);
+		test.affiche();
+		test.deplace(6,7);
+		test.affiche();
+		test.compare(4);
+		test.surface();	
+		
+		//Les méthodes sont : 
+		//C_CERCLE (abs, ord, rayon)
+		//déplace (delta_x, delta_y)
+		//affiche () // utilisez un simple appel à « println »
+		//boolean compare ( cercle ) // comparaison des rayons uniquement
+		//double surface()
+		
+		//exo2
+//		Créez un tableau de 3 objets de la classe C_CERCLE définie.
+		C_CERCLE[]test2=new C_CERCLE[3];
+		test2[0]=new C_CERCLE(5, 10, 5);
+//		test2[0].setX(5);
+//		test2[0].setY(10);
+//		test2[0].setR(5);
+		test2[1]=new C_CERCLE(4, 8, 3);
+//		test2[1].setX(4);
+//		test2[1].setY(8);
+//		test2[1].setR(3);
+		test2[2]=new C_CERCLE(8, 9, 10);
+//		test2[2].setX(8);
+//		test2[2].setY(9);
+//		test2[2].setR(10);
+		for(int i=0;i<test2.length;i++) {
+			int r=i+1;
+		System.out.println("Objet "+ r);
+		System.out.println("X: "+test2[i].getX());	
+		System.out.println("Y: "+test2[i].getY());
+		System.out.println("R: "+test2[i].getR());
+		}
+		
+	}
+	
+}
